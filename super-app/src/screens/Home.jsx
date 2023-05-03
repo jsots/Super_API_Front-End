995432a115032ec910e9807f123c05273f0f8d0c
Jacquelyn <jaxlynhyde@gmail.com>
2023-05-03 15:05:50 -0400

    modified home.jsx and created home.css

--- super-app/src/screens/Home.jsx
+++ super-app/src/screens/Home.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import Characters from './Characters'
+import React from "react";
+import Characters from "./Characters";
+import "./Home.css";
+
 function Home() {
   return (
     <div>
-      <Characters />
+      <article className="comic">
+      <h1>Celestial Characters</h1>
+  <div className="panel">
+    <p className="text bottom-right"><a href="/characters">Characters</a></p>
+  </div>
+  <div className="panel">
+    <p className="text top-left">Try resizing...</p>
+    <p className="text bottom-right">...it's responsive</p>
+  </div>
+  <div className="panel">
+  </div>
+  <div className="panel"></div>
+  <div className="panel"></div>
+    <p className="text top-left"></p>
+  <div className="panel"></div>
+  <div className="panel">
+    <p className="speech">A speech bubble</p></div>
+  <div className="panel"></div>
+  <div className="panel">
+    <p className="text bottom-right">THE END</p>
+  </div>
+</article>
     </div>
-  )
+  );
 }
 
 export default Home;

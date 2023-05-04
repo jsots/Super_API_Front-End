2a14f6b94bd69c9c7e0d1f6f35bda2b9c4cdf4da
“Lissa <lwarshaw674@gmail.com>
2023-05-04 13:01:46 -0400

    verify user headers added

--- super-app/src/services/apiConfig.js
+++ super-app/src/services/apiConfig.js
@@ -17,4 +17,17 @@ const api = axios.create({
   baseURL: apiUrl,
 });
 
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
 export default api;

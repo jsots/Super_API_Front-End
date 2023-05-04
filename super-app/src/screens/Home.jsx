110081b353a854fb3af898c9c291d6ec8b94d73d
Jacquelyn <jaxlynhyde@gmail.com>
2023-05-04 18:09:39 -0400

    88

--- super-app/src/screens/Home.jsx
+++ super-app/src/screens/Home.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useEffect } from "react"
 
 function Home() {
+
+  useEffect(() => {
+
+    document.body.classList.add("home")
+
+    return () => {
+      document.body.classList.remove("home")
+    }
+  }, [])
+
+
   return (
     <div id="home">
       <h1>Celeste's Supers</h1>

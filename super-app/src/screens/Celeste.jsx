c93804478ad8df84107eb5d4d9d7ae3d1d18cc05
jsots <justinsotolongo@gmail.com>
2023-05-05 09:44:33 -0400

    Meet celeste works, needs images

--- super-app/src/screens/Celeste.jsx
+++ super-app/src/screens/Celeste.jsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom"
+
 function Celeste() {
   return (
     <div>
@@ -5,7 +7,7 @@ function Celeste() {
 
       <div>
         <p>Justin Sotolongo</p>
-        <img src="" width="200" height="200"></img>
+        {/* <img src="" width="200" height="200"></img> */}
         <p>Hero Name: Agentgay</p>
         <p>Fav Hero : Raven</p>
         <p>Powerstats</p>
@@ -17,13 +19,13 @@ function Celeste() {
           <li>Power:5</li>
           <li>Combat:0</li>
         </ul>
-        <link href="https://github.com/jsots">link to github</link>
+        <Link to="https://github.com/jsots">link to github</Link>
         <p>work: code czar </p>
       </div>
 
       <div>
         <p>Lissa Warshaw</p>
-        <img src="" width="200" height="200"></img>
+        {/* <img src="" width="200" height="200"></img> */}
         <p>Hero Name : Luminous Lissa</p>
         <p>Fav Hero : Andy Cohen</p>
         <p>Powerstats</p>
@@ -35,13 +37,13 @@ function Celeste() {
           <li>Power:100</li>
           <li>Combat:10</li>
         </ul>
-        <link href="https://github.com/lissashea">link to my github</link>
+        <Link to="https://github.com/lissashea">link to my github</Link>
         <p>Work: GA Software Engineer Student </p>
       </div>
 
       <div>
         <p>Jacquelyn Caputo</p>
-        <img src="" width="200" height="200"></img>
+        {/* <img src="" width="200" height="200"></img> */}
         <p>Heroname : Ashwaganda</p>
         <p>Favhero : Star Fire</p>
         <p>Powerstats</p>
@@ -53,13 +55,13 @@ function Celeste() {
           <li>Power: 80</li>
           <li>Combat: 77</li>
         </ul>
-        <link href="https://github.com/professrx/">link to my github</link>
+        <Link to="https://github.com/professrx/">link to my github</Link>
         <p>work: Veterinarian </p>
       </div>
 
       <div>
         <p>Michael Christenson</p>
-        <img src="" width="200" height="200"></img>
+        {/* <img src="" width="200" height="200"></img> */}
         <p>Hank Chinaski</p>
         <p>Favhero</p>
         <p>Powerstats</p>
@@ -71,13 +73,13 @@ function Celeste() {
           <li>Power: 2 </li>
           <li>Combat:25</li>
         </ul>
-        <link href="https://github.com/mcchris1">link to my github</link>
+        <Link to="https://github.com/mcchris1">link to my github</Link>
         <p>work: D-Girl/Group Inclusion Specialist</p>
       </div>
 
       <div>
         <p>Agustines Sudibya</p>
-        <img src="" width="200" height="200"></img>
+        {/* <img src="" width="200" height="200"></img> */}
         <p>Simu Liu,</p>
         <p>Shang-Chi</p>
         <p>Powerstats</p>
@@ -89,7 +91,7 @@ function Celeste() {
           <li>Power: 50</li>
           <li>Combat: 99</li>
         </ul>
-        <link href="https://github.com/ksudibya">link to my github</link>
+        <Link to="https://github.com/ksudibya">link to my github</Link>
         <p>work: Software Engineer</p>
       </div>
     </div>
